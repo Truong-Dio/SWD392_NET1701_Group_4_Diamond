1137d84e67c5639960fef42457873a66b2249abd
MinhKQ <minhkieu702@gmail.com>
2024-07-11 14:17:27 +0700

    add webjobs

---
+++ DiamondStoreSystem/DiamondStoreSystem.MyWebJobs/Program.cs
@@ -0,0 +1,24 @@
+﻿using DiamondStoreSystem.BusinessLayer.IServices;
+using DiamondStoreSystem.BusinessLayer.Services;
+using DiamondStoreSystem.MyWebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = new HostBuilder();
+builder.ConfigureWebJobs(b =>
+{
+    b.AddAzureStorageCoreServices();
+    b.AddTimers();
+});
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddSingleton<IWarrantyService, WarrantyService>();
+    services.AddSingleton<Functions>();
+});
+
+var host = builder.Build();
+using (host)
+{
+    await host.RunAsync();
+}
--- DiamondStoreSystem/DiamondStoreSystem.WebJob/Functions.cs
+++ DiamondStoreSystem/DiamondStoreSystem.WebJob/Functions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,10 @@ namespace DiamondStoreSystem.WebJob
     {
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
+        public Functions(IWarr)
+        {
+            
+        }
         public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
         {
             log.WriteLine(message);
