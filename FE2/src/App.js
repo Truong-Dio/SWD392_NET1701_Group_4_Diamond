0bb1f71873017f7dcb64a2546de28dd54f5f53e7
Truong-Dio <85091699+Truong-Dio@users.noreply.github.com>
2024-07-11 20:17:10 +0700

    set_up firebase

---
+++ FE2/src/App.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import Header from './components/Header';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css'; 
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <div className="container">
+//         <h1 className="my-4">Staff Page</h1>
+//         <p></p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
---
+++ FE2/src/back-button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/backButton.css'; 
+
+const BackButton = () => {
+  return (
+    <Link to="#" className="button">Quay lại</Link>
+  );
+};
+
+export default BackButton;
