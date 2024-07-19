c23f5cb6b5c7a74377c60ffb31c5bab583f2fa08
thienNN3011 <144593901+thienNN3011@users.noreply.github.com>
2024-07-19 08:21:53 +0700

    Delete src/staff directory
    delete file cu
    
    Signed-off-by: thienNN3011 <144593901+thienNN3011@users.noreply.github.com>

--- src/staff/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ element: Component, isLoggedIn, ...rest }) => {
-  return isLoggedIn ? <Component {...rest} /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
